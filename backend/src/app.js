5b1ec0d778b0217382f2896a372c735387228ccb
sachinmags5 <sachinmags5@gmail.com>
2026-02-20 03:59:51 +0530

    added rate limiter and helmet for security

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 import userRoutes from "./modules/user/user.routes.js";
 import categoryRoutes from "./modules/category/category.routes.js";
 import transactionRoutes from "./modules/transaction/transaction.routes.js";
@@ -18,6 +19,16 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
+// app.use(helmet());
+//to remove  x-powered-by
+app.disable("x-powered-by");
+app.use(
+  helmet({
+    xPoweredBy: false,
+  }),
+);
+app.set("trust proxy", 1);
+
 app.use("/api/users", userRoutes);
 app.use("/api/category", authenticate, categoryRoutes);
 app.use("/api/transaction", authenticate, transactionRoutes);
